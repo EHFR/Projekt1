6a8b285269b1fd6a779021802810314ef21c48f5
EmrArc <emre_arduc@outlook.de>
2018-06-11 17:26:39 +0200

    asdasd

--- src/ELAB/Fertigungsverwaltung.java
+++ src/ELAB/Fertigungsverwaltung.java
@@ -228,8 +228,9 @@ public class Fertigungsverwaltung {
         
         
         String sql = "UPDATE Auftrag SET Titel = '" + titel + "', FertigungsArt = '" + fertigungsArt
-                + "', DateiName = '" + dateiName + "', DateiOrt = '" + dateiOrt + "', Kosten = '" + kostenFloat + ",'" + pw.getPersonIdByName(auftraggeber)
-                + "' WHERE ID = " + id + "";
+                + "', DateiName = '" + dateiName + "', DateiOrt = '" + dateiOrt + "', Kosten = " + kostenFloat + ", AuftraggeberID = " + pw.getPersonIdByName(auftraggeber)
+                + " WHERE ID = " + id + "";
+        System.out.println(sql);
         try {
             db.updateQuery(sql);
         } catch (SQLException e) {
